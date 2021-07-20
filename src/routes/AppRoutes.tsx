d71da859f58067b41f888b848841b66c67b3c415
Saul Ramirez <fernando.ramire@hotmail.com>
2021-07-19 19:45:11 -0600

    Adding button to toggle drawer

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
+import { DrawerActions, NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AboutPage from '../pages/AboutPage';
 import DetailPage from '../pages/DetailPage';
 import HomePage from '../pages/HomePage';
 import SettingsPage from '../pages/SettingsPage';
 import NotificationsPage from '../pages/NotificationsPage';
+import { Button, Text, View } from 'react-native';
 
 
 const Tabs = createBottomTabNavigator();
@@ -15,10 +16,12 @@ const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
 
+
 const AppDrawer = () => {
   return (
-    <Drawer.Navigator initialRouteName="Home">
-      <Drawer.Screen name="Home1" component={AppTabs} />
+
+    <Drawer.Navigator initialRouteName="Home"  >
+      <Drawer.Screen name="Home" component={AppTabs} />
       <Drawer.Screen name="Notifications" component={NotificationsPage} />
     </Drawer.Navigator>
   );
@@ -37,16 +40,24 @@ const AppTabs = () => {
 const AppRoutes = () => {
 
   return (
-
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Detail" component={DetailPage} />
         <Stack.Screen name="About" component={AboutPage} />
-        <Stack.Screen name="Home" component={AppDrawer} />
+        <Stack.Screen
+          name="Home"
+          component={AppDrawer}
+          options={({ navigation }) => ({
+            headerLeft: () => (
+              <Button
+                onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+                title="Menu"
+              />
+            ),
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-
-
   )
 }
 
